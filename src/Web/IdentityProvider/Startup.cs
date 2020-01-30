23487119dde7e639218d2548075ff3201dfbbb46
Alex Tochetto <alextochetto@gmail.com>
2020-01-30 20:21:55 -0300

    identity server in memory

--- src/Web/IdentityProvider/Startup.cs
+++ src/Web/IdentityProvider/Startup.cs
@@ -1,9 +1,11 @@
+﻿using IdentityServer4.Configuration;
 using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Cryptography.X509Certificates;
 
 namespace IdentityProvider
 {
@@ -37,10 +39,19 @@ namespace IdentityProvider
             identityServer.AddInMemoryApiResources(_configuration.GetSection(nameof(ApiResource)));
             identityServer.AddInMemoryClients(_configuration.GetSection(nameof(Client)));
 
+            //var certThumbPrint = "‎‎CF4FBD5BE8CBEADF923DBE65ADC6780339914ADA";
+            //var certStore = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            // Try to open the store.
+
+            //certStore.Open(OpenFlags.ReadOnly);
+            // Find the certificate that matches the thumbprint.
+            //var certCollection = certStore.Certificates.Find(X509FindType.FindByThumbprint, certThumbPrint, false);
+            //certStore.Close();
+
             if (_webHostEnvironment.IsDevelopment())
                 identityServer.AddDeveloperSigningCredential();
-            //else
-            //    identityServer.AddSigningCredential("6AA95D1EA9DFD6B5E4029D165DBA499D8DC1BB50", StoreLocation.CurrentUser, NameType.Thumbprint);
+            else
+                identityServer.AddSigningCredential("4DFF9B8EBB5314B9A62EFA72DA8B4D7658231C05", StoreLocation.CurrentUser, NameType.Thumbprint);
 
             services.AddAuthentication();
         }
@@ -54,7 +65,7 @@ namespace IdentityProvider
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
