fa8f06b4a456a8ed5a5e37cd1a8e21bcb9a6adb7
Alex Tochetto <alextochetto@outlook.com>
2020-02-21 12:30:22 -0300

    identity server updated at the same branch of payment api

--- src/Web/IdentityProvider/Startup.cs
+++ src/Web/IdentityProvider/Startup.cs
@@ -1,9 +1,12 @@
+﻿using IdentityServer4.Configuration;
 using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Security.Cryptography.X509Certificates;
+using IdentityServer.Extensions;
 
 namespace IdentityProvider
 {
@@ -31,16 +34,31 @@ namespace IdentityProvider
             //identityServer.AddInMemoryIdentityResources(Configuration.Config.Ids);
             //identityServer.AddInMemoryApiResources(Configuration.Config.Apis);
             //identityServer.AddInMemoryClients(Configuration.Config.Clients);
-            //.AddAspNetIdentity<ApplicationUser>();
 
             identityServer.AddInMemoryIdentityResources(_configuration.GetSection(nameof(IdentityResources)));
             identityServer.AddInMemoryApiResources(_configuration.GetSection(nameof(ApiResource)));
             identityServer.AddInMemoryClients(_configuration.GetSection(nameof(Client)));
 
+            #region Thumbprint
             if (_webHostEnvironment.IsDevelopment())
                 identityServer.AddDeveloperSigningCredential();
+            else
+                identityServer.AddSigningCredential("4DFF9B8EBB5314B9A62EFA72DA8B4D7658231C05", StoreLocation.CurrentUser, NameType.Thumbprint);
+            #endregion
+
+            #region Uso do *.pfx direto
+            //if (_webHostEnvironment.IsDevelopment())
+            //    identityServer.AddDeveloperSigningCredential();
             //else
-            //    identityServer.AddSigningCredential("6AA95D1EA9DFD6B5E4029D165DBA499D8DC1BB50", StoreLocation.CurrentUser, NameType.Thumbprint);
+            //    identityServer.AddSigningCredential(new X509Certificate2("certificate.pfx", "987654321")); 
+            #endregion
+
+            #region Forma de uso maqueado
+            //if (_webHostEnvironment.IsDevelopment())
+            //    identityServer.AddDeveloperSigningCredential();
+            //else
+            //    identityServer.AddCustomSigningCredential();
+            #endregion
 
             services.AddAuthentication();
         }
@@ -54,7 +72,7 @@ namespace IdentityProvider
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
                 app.UseHttpsRedirection();
