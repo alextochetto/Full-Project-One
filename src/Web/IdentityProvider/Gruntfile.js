881f9e45076a5a94c7ddbf057d1674888e2ed8a3
Alex Tochetto <alextochetto@outlook.com>
2020-03-09 15:14:38 -0300

    npm and grunt configuration

---
+++ src/Web/IdentityProvider/Gruntfile.js
@@ -0,0 +1,25 @@
+﻿/// <binding BeforeBuild='all' />
+module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: ["wwwroot/lib/*"],
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'node_modules/',
+                src: [
+                    'bootstrap/**/*',
+                    'jquery/dist/**/*'
+                ],
+                dest: 'wwwroot/lib/',
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask("all", ['clean', 'copy']);
+};
