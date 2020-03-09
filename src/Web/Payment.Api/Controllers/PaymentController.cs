82549b8e4c7d1b9b8e4a7946aa0159fe8f7380eb
Alex Tochetto <alextochetto@outlook.com>
2020-03-09 13:24:54 -0300

    copied api

---
+++ src/Web/Payment.Api/Controllers/PaymentController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Payment.Api.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
